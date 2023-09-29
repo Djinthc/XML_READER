c8e9e9dec40aa7ea1a02413c838e92e7bf5a9c1f
Michał Kaszuba <michalkaszuba00@gmail.com>
2023-09-29 15:00:44 +0200

    canva_s

--- statement_class.py
+++ statement_class.py
@@ -5,6 +5,61 @@ class Statement:
     '''   IN BUILD!    '''
     xmlfile = ''
 
+    canva = f'''
+-----------------------------------------------------------------------------------------------------------------------
+|                                   SPRAWOZDANIE FINANSOWE / FINANCIAL STATEMENT                                      |
+-----------------------------------------------------------------------------------------------------------------------
+|DATA / DATE                                                                                                          |
+-----------------------------------------------------------------------------------------------------------------------
+|NAZWA FIRMY / COMPANY NAME                                                                                           |
+-----------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------
+|                                                  BILANS / BALANCE                                                   |
+-----------------------------------------------------------------------------------------------------------------------
+|AKTYWA                                                                                                               |
+-----------------------------------------------------------------------------------------------------------------------
+| A. AKTYWA TRWAŁE                                                                                                    |
+-----------------------------------------------------------------------------------------------------------------------
+|    I. WARTOŚCI NIEMATERIALNE I PRAWNE                                                                               |
+|       1. KOSZTY ZAKOŃCZONYCH PRAC ROZWOJOWYCH                                                                       |
+|       2. WARTOŚĆ FIRMY                                                                                              |
+|       3. INNE WARTOŚCI NIEMATERIALNE I PRAWNE                                                                       |
+|       4. ZALICZKI NA WARTOŚCI NIEMATERIALNE I PRAWNE                                                                |
+-----------------------------------------------------------------------------------------------------------------------
+|   II. RZECZOWE AKTYWA TRWAŁE                                                                                        | 
+|       1. ŚRODKI TRWAŁE                                                                                              |
+|           a) grunty (w tym prawo użytkowania gruntów)                                                               |
+|           b) budynki, lokale, prawa do lokali i obiekty inżynierii lądowej i wodnej                                 |
+|           c) urządzenia techniczne i maszyny                                                                        |
+|           d) środki transportu                                                                                      |
+|           e) inne środki trwałe                                                                                     |
+|       2. ŚRODKI TRWAŁE W BUDOWIE                                                                                    |
+|       3. ZALICZKI NA ŚRODKI TRWAŁE W BUDOWIE                                                                        |
+-----------------------------------------------------------------------------------------------------------------------
+|   III. NALEŻNOŚCI DŁUGOTERMINOWE                                                                                    |
+|       1. OD JEDNOSTEK POWIĄZANYCH                                                                                   |
+|       2. OD POZOSTAŁYCH JEDNOSTEK, W KTÓRYCH JEDNOSTKA POSIADA ZAANGAŻOWANIE W KAPITALE                             |
+|       3. OD POZOSTAŁYCH JEDNOSTEK                                                                                   |
+-----------------------------------------------------------------------------------------------------------------------
+|   IV. INWESTYCJE DŁUGOTERMINOWE
+|       1. NIERUCHOMOŚCI
+|       2. WARTOŚCI NIEMATERIALNE I PRAWNE
+|       3. DŁUGOTERMIONWE AKTYWA FINANSOWE
+|           a) w jednostkach powiązanych
+|               - udziały lub akcje
+|               - inne papiery wartościowe
+|               - udzielone pożyczki
+|               - inne długotermionowe aktywa finansowe
+|           b) w pozostałych jednostkach, w których jednostka posiada zaangażowanie w kapitale
+|               - udziały lub akcje
+|               - inne papiery wartościowe
+|               - udzielone pożyczki
+|               - inne długotermionowe aktywa finansowe
+|
+|
+'''
+    
+
     def __init__(self):
         pass
 
@@ -47,7 +102,7 @@ class Statement:
             with open(file_path, "r") as file:
                 Statement.xmlfile = file.read()
             print('File has been successfully loaded.')
-            return 
+            return
         except:
             print('Loading went wrong. Make sure statement file name is correct. Try again')
             Statement.load(self)
